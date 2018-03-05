e6e22523dd0c9d00cb6377605ca0882dbc965df1
changyue.li <changyue.li@tendcloud.com>
2018-03-05 17:18:06 +0800

    KNN

--- ML/CH2/KNN.py
+++ ML/CH2/KNN.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = 'tend'
 from numpy import *
 import operator
@@ -40,6 +41,126 @@ print labels
 # execute classify method
 c =classify0([0,0],group,labels,3)
 print c #B
+
+
+################################################################################
+#2.2
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index ,:] = listFromLine[0:3] #选择3个字段
+        classLabelVector.append(int(listFromLine[-1]))
+        index +=1
+    return returnMat,classLabelVector
+
+
+
+
+
+
+#
+datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+
+print datingDataMat
+
+print datingLabels[0:20]
+
+#输出散点图
+import matplotlib
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingLabels),15.0 * array(datingLabels))
+#plt.show()
+
+
+#散点图2
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#里程数和百分比作图
+ax.scatter(datingDataMat[:,0],datingDataMat[:,1],15.0*array(datingLabels),15.0 * array(datingLabels))
+#plt.show()
+
+
+
+
+#归一化
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet)) #生成shape 与dataSet一样的0矩阵
+    m = dataSet.shape[0]
+    normDataSet = dataSet-tile(minVals ,(m,1)) #tile表示复制数组
+    normDataSet = normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
+
+
+normMat,ranges,minVals = autoNorm(datingDataMat)
+
+
+
+#测试算法效果
+
+def datingClassTest():
+
+    hoRatio = 0.10
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat,ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0] #行数
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:], datingLabels[numTestVecs:m],3)
+        print "the calssifier came back with: %d,the real answer is: %d" % (classifierResult,datingLabels[i])
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+        print "the total error rate is : %f" % (errorCount /float(numTestVecs))
+
+
+
+#执行
+#datingClassTest()
+
+
+
+
+
+def classifyPersion():
+    resultList = ['not at all','in small dases','in large doses']
+    percentTats = float(raw_input(
+        "percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat ,ranges,minVals  = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats,iceCream])
+    classifierResult = classify0((inArr - minVals)/ranges,normMat,datingLabels,3)
+    print "You weill probably like this persion:",resultList[classifierResult -1]
+
+
+
+classifyPersion()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
