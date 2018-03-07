7089da2fa4c179787fee7295048d5702b9174dc8
changyue.li <changyue.li@tendcloud.com>
2018-03-07 16:20:35 +0800

    KNN

--- ML/CH2/KNN.py
+++ ML/CH2/KNN.py
@@ -201,10 +201,39 @@ def handwritingClassTest():
     print  "\nthe totla number of errors is : %d" % errorCount
     print "\nthe total error rate is : %f" % (errorCount /float(mTest))
 
+def  handwritingClassTest2():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr =  int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList= listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr =int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+
+        classifierResult = classify0(vectorUnderTest, trainingMat,hwLabels,5)
+        print "the classifier came back with :%d , the real answer is : %d" % (classifierResult,classNumStr)
+
+        if ( classifierResult != classNumStr) : errorCount +=1.0
+    print "\n the total number of errors is :%d" % errorCount
+    print "\n the total error rate is : %f" %(errorCount/float(mTest))
+
+
 
 #执行
 handwritingClassTest()
 
+handwritingClassTest2()
 
 
 
