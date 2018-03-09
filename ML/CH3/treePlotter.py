cc57bacb7e5a36136d0f4f6a30c53f2a09daa938
changyue.li <changyue.li@tendcloud.com>
2018-03-09 17:10:20 +0800

    trees

--- ML/CH3/treePlotter.py
+++ ML/CH3/treePlotter.py
@@ -1,1 +1,46 @@
+# -*- coding: utf-8 -*-
 __author__ = 'tend'
+
+import matplotlib.pyplot as plt
+
+
+decisionNode = dict(boxstytle ="sawtooth",fc ="0.8")
+leafNode = dict(boxstyle="round4",fc="0.8")
+arrow_args=dict(arrowstyle="<-")
+
+
+def createPlot():
+    fig = plt.figure(1,facecolor='white')
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111,frameon =False)
+    plotNode('a decision node',(0.5,0.1),(0.1,0.5),decisionNode)
+    plotNode('a leaf node',(0.8,0.1),(0.3,0.8),leafNode)
+    plt.show()
+
+
+def plotNode(nodeTxt,centerPt,parentPt,nodeType):
+    createPlot.ax1.annotate(nodeTxt,xy=parentPt,xycoords='axes fraction',
+                            xytext=centerPt,textcoords='axes fraction',
+                            va ="center",ha="center",bbox=nodeType,arrowprops=arrow_args)
+
+
+
+
+#执行
+createPlot()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
