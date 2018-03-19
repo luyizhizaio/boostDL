50262dfc1b32ca3e3f02ece760a4e17c225d6884
changyue.li <changyue.li@tendcloud.com>
2018-03-19 18:29:19 +0800

    logisticRegression

--- ML/CH5/logRegres.py
+++ ML/CH5/logRegres.py
@@ -77,7 +77,7 @@ def plotBestFit(weights):
 
 
 #执行
-plotBestFit(weight.getA())
+#plotBestFit(weight.getA())
 
 
 
@@ -101,7 +101,7 @@ weights = stocGradAscent0(array(dataMat),labelMat)
 
 print 'stocGradAscent'
 print weights
-plotBestFit(weights)
+#plotBestFit(weights)
 
 
 
@@ -132,9 +132,49 @@ weights = stocGradAscent1(array(dataMat),labelMat)
 
 print  weights
 
-plotBestFit(weights)
+#plotBestFit(weights)
 
 
+
+#分类函数
+def classifyVector(inX,weights):
+    prob=sigmoid(sum(inX *weights))
+    if prod > 0.5: return 1.0
+    else: return 0.0
+
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet=[]; trainingLabels=[]
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr= []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(array(trainingSet),trainingLabels,500)
+    errorCount = 0; numTestVec=0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0 #测试样本总数
+        currLine = line.strip().split('\t')
+        if int(classifyVector(array(lineArr),trainWeights))!=int(currLine[21]):
+            errorCount +=1
+    errorRate = (float(errorCount)/numTestVec)
+    print "the error rate of this test is: %f" % errorRate
+    return errorRate
+
+def multiTest():
+    #调用10次取结果的平均值。
+    numTests=10;errorSum=0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print "after %d iterations the average error rate is:%f" % (numTests,errorSum/float(numTests))
+
+
+#execute
+multiTest()
 
 
 
