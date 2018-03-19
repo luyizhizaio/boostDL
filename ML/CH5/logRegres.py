c651cf7e5ca242dc281e515a7ada8294af6eae7a
changyue.li <changyue.li@tendcloud.com>
2018-03-19 16:37:07 +0800

    trees

--- ML/CH5/logRegres.py
+++ ML/CH5/logRegres.py
@@ -46,3 +46,35 @@ weight = gradAscent(dataArr,labelMat)
 
 print weight
 
+
+#画出数据集的最佳拟合函数
+def plotBestFit(wei):
+    import matplotlib.pyplot as plt
+    weights =wei.getA() #返回数组
+    dataMat,labelMat = loadDataSet()
+    dataArr=array(dataMat) #转成数组
+    n = shape(dataArr)[0] #数据集行数
+    xcord1 =[]; ycord1 = []
+    xcord2 = [];ycord2=[]
+    for i in range(n):
+        if int(labelMat[i]) ==1:
+            xcord1.append(dataArr[i,1]);
+            ycord1.append(dataArr[i,2]);
+        else:
+            xcord2.append(dataArr[i,1]);
+            ycord2.append(dataArr[i,2]);
+    fig = plt.figure()
+    ax = fig.add_subplot(111) #子图
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s') #算点图
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+
+    x = arange(-3.0,3.0,0.1) #返回-3到3的数组
+    y = (-weights[0] -weights[1] *x)/weights[2] #ax+by+c =0 的直线
+    ax.plot(x,y)
+    plt.xlabel('X1');plt.ylabel('X2')
+    plt.show()
+
+
+
+#执行
+plotBestFit(weight)
